35957f2caa486fa6ae3362ee9dde545e6bab52c2
Demodex420 <bastian.siegmund@icloud.com>
2022-01-20 12:01:43 +0100

    merged

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Item from "./components/Item";
 
 
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <div className="header-icons">[]icons[]</div>
+        <div className="header-text">
+          <h1>Pick 'Em</h1>
+
+          <h2>Shopping List App</h2>
+        </div>
+        <div className="header-icons">[]icons[]</div>
+      </header>
+      <main>
+        <section className="your-list-box">
+          <div className="your-list-header">
+            <h3>Your list:</h3>
+            <p className="calories">Total Calories: XXXX</p>
+          </div>
+          <div className="your-list-items">
+            <Item />
+          </div>
+        </section>
+        <section className="all-items-box">
+          <h3>All items:</h3>
+
+          <div className="all-items">
+            <Item />
+          </div>
+        </section>
+      </main>
+      <footer>©2022 Crossover Team 1</footer>
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,10 @@
+const Item = () => {
+  return (
+    <li>
+      <img className="item-icon" src="" />
+      <p className="item-name">Item Name</p>
+    </li>
+  );
+};
+
+export default Item;
